46eeb803c96c778e43a49128fadc5fd5aae592d9
hanshitrash <110578742+hanshitrash@users.noreply.github.com>
2022-11-27 22:10:44 +0800

    api open weather
    1 api aja

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
-import numpy
 
 
-API_KEY = "d485793245775bad0112e296db963d3a"
+API_KEY = "bab49de27682105d94789820f4404b5e"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+BASE_URL2 = "http://api.openweathermap.org/data/2.5/forecast"
 namaKota = input("Nama kota : ")
 request_url = f"{BASE_URL}?appid={API_KEY}&q={namaKota}&lang=id"
 respone = requests.get(request_url)
@@ -11,73 +11,57 @@ data = respone.json()
 lon = data['coord']['lon'] #longitude
 lat = data['coord']['lat'] #latitude
 print(lat, lon)
+request_url2 = f"{BASE_URL2}?lat={lat}&lon={lon}&appid={API_KEY}&lang=id"
+respon = requests.get(request_url2)
+data2 = respon.json()
 
-request_url2 = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m,relativehumidity_2m,precipitation,weathercode,surface_pressure,cloudcover&current_weather=true&timezone=Asia%2FSingapore"
-respone2 = requests.get(request_url2)
+list = data2['list']
 
-if respone2.status_code == 200:
-    data2 = respone2.json()
-    weather_now = data2['current_weather']['temperature']
-    waktu = data2['current_weather']['time']
-    perjam = data2['hourly']['time']
-    listCuacaPerjam = []
-    cuaca_perjam = data2['hourly']['temperature_2m']
-    for i in range(len(cuaca_perjam)):
-        cuaca_perjam[i] = str(cuaca_perjam[i])
-        listCuacaPerjam.append(cuaca_perjam[i] + " C")
+def listWaktu(n):
+    list_waktu = []
+    for i in range(len(list)):
+        list_waktu.append(list[i]['dt_txt'])
+    print(list_waktu[n])
+listWaktu(2)
 
-    listKelembaban = []
-    kelembaban = data2['hourly']['relativehumidity_2m']
-    for i in range(len(kelembaban)):
-        kelembaban[i] = str(kelembaban[i])
-        listKelembaban.append(kelembaban[i] + " %")
+def listTemperature(n):    
+    list_temp = []
+    for i in range(len(list)):
+        list_temp.append(round(list[i]['main']['temp'] - 273.15, 1))
+    print(list_temp[n])
+listTemperature(2)
 
-    listPresipitasi = []
-    precipitasi = data2['hourly']['precipitation']
-    for i in range(len(precipitasi)):
-        precipitasi[i] = str(precipitasi[i])
-        listPresipitasi.append(precipitasi[i] + " mm")
+def listPressure(n):
+    list_pressure = []
+    for i in range(len(list)):
+        list_pressure.append(list[i]['main']['pressure'])
+    print(list_pressure[n])
+listPressure(2)
 
-    listTekananUdara = []
-    tekanan_permukaan = data2['hourly']['surface_pressure']
-    for i in range(len(tekanan_permukaan)):
-        tekanan_permukaan[i] = str(tekanan_permukaan[i])
-        listTekananUdara.append(tekanan_permukaan[i] + " Pa")
-        
-    kode_cuaca = data2['hourly']['weathercode']
-    for i in range(len(kode_cuaca)):
-        if kode_cuaca[i] == 0:
-            kode_cuaca[i] = "Langit cerah"
-        elif kode_cuaca[i] == 2:
-            kode_cuaca[i] = "Sedikit berawan"
-        elif kode_cuaca[i] == 3:
-            kode_cuaca[i] = "Mendung"
-        elif kode_cuaca[i] == 45 or kode_cuaca[i] == 48:
-            kode_cuaca[i] = "Berkabut"
-        elif kode_cuaca[i] == 51 or kode_cuaca[i] == 53 or kode_cuaca[i] == 55:
-            kode_cuaca[i] = "Rintik-rintik"
-        elif kode_cuaca[i] == 56 or kode_cuaca[i] == 57:
-            kode_cuaca[i] = "Rintik-rintik, sedikit bersalju"
-        elif kode_cuaca[i] == 61 or kode_cuaca[i] == 63 or kode_cuaca[i] == 65:
-            kode_cuaca[i] = "Gerimis"
-        elif kode_cuaca[i] == 66 or kode_cuaca[i] == 67:
-            kode_cuaca[i] = "Gerimis, sedikit bersalju"
-        elif kode_cuaca[i] == 71 or kode_cuaca[i] == 73 or kode_cuaca[i] == 75:
-            kode_cuaca[i] = "Sedikit bersalju"
-        elif kode_cuaca[i] == 77:
-            kode_cuaca[i] = "Butiran salju turun"
-        elif kode_cuaca[i] == 80 or kode_cuaca[i] == 81 or kode_cuaca[i] == 82:
-            kode_cuaca[i] = "Hujan ringan"
-        elif kode_cuaca[i] == 85 or kode_cuaca[i] == 86:
-            kode_cuaca[i] = "Hujan salju ringan"
-        elif kode_cuaca[i] == 95:
-            kode_cuaca[i] = "Hujan lebat disertai petir"
-        elif kode_cuaca[i] == 96 or kode_cuaca[i] == 99:
-            kode_cuaca[i] = "Hujan lebat disertai hujan es" #Only available in central europe
-    
-    cuaca_seminggu = numpy.column_stack([perjam, listCuacaPerjam, listKelembaban, listPresipitasi, listTekananUdara, kode_cuaca])
-    print(f"Sekarang: {waktu}, Cuaca: {weather_now} derajat Celcius")
-    print("ini cuaca sehari kelipatan 3\n",cuaca_seminggu[:24:3])
-    print("ini cuaca seminggu\n",cuaca_seminggu[::24])
-else:
-    print("An error occured")
+def listHumidity(n):
+    list_humidity = []
+    for i in range(len(list)):
+        list_humidity.append(list[i]['main']['humidity'])
+    print(list_humidity[n])
+listHumidity(2)
+
+def listWeatherMain(n):
+    list_weather_main = []
+    for i in range(len(list)):
+        list_weather_main.append(list[i]['weather'][0]['main'])
+    print(list_weather_main[n])
+#listWeatherMain(0) #Kalo perlu aja mad
+
+def listWeatherDesc(n):
+    list_weather_description = []
+    for i in range(len(list)):
+        list_weather_description.append(list[i]['weather'][0]['description'])
+    print(list_weather_description[n])
+listWeatherDesc(2)
+
+def listWeatherIcon(n):
+    list_weather_icon = []
+    for i in range(len(list)):
+        list_weather_icon.append(list[i]['weather'][0]['icon'])
+    print(list_weather_icon[n])
+listWeatherIcon(2)
